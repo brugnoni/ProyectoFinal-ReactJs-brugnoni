f68d9676c49f291ea88ba9f6795e59fa473cb8e7
Nahuel Brugnoni <nahubrugnoni@gmail.com>
2023-04-03 03:28:05 -0300

    Cambios y mejoras al proyecto

--- src/Components/ItemCount/ItemCount.jsx
+++ src/Components/ItemCount/ItemCount.jsx
@@ -1,13 +1,13 @@
 import { Button } from "@mui/material";
 import { useEffect, useState } from "react";
-import "./ItemCount.css";
+import "./ItemCount.css"
 
-const ItemCount = ({ stock, initial = 1, onAdd }) => {
+const ItemCount = ({ stock, initial=1, onAdd }) => {
   const [contador, setContador] = useState(initial);
 
-  useEffect(() => {
-    setContador(initial);
-  }, [initial]);
+  useEffect(()=>{
+  setContador(initial)
+  }, [initial])
 
   const sumar = () => {
     if (contador < stock) {
@@ -23,23 +23,19 @@ const ItemCount = ({ stock, initial = 1, onAdd }) => {
 
   return (
     <div className="container-btn">
-      <h2 style={{ fontFamily: "roboto" }}>Cantidad: {contador}</h2>
-      <div className="btns">
-        <Button variant="outlined" onClick={sumar}>
-          +
-        </Button>
-        <Button
-          style={{ fontFamily: "roboto" }}
-          variant="contained"
-          onClick={() => onAdd(contador)}
-        >
-          Agregar al carrito
-        </Button>
-        <Button variant="outlined" onClick={restar}>
-          -
-        </Button>
-      </div>
+    <h2>Cantidad: {contador}</h2>
+    <div className="btns">
+      <Button variant="outlined" onClick={sumar}>
+        +
+      </Button>
+      <Button variant="contained" onClick={() => onAdd(contador)}>
+        agregar al carrito
+      </Button>
+      <Button variant="outlined" onClick={restar}>
+        -
+      </Button>
     </div>
+  </div>
   );
 };
 
